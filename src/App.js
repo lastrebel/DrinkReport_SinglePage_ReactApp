6eb85c5d6682ba7bdd37973c469c45c115051980
‘Brano <valasek.brano@gmail.com>
2021-06-23 21:08:18 +0200

    first commit - react app installed and cleaned

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>hellou hellou hellou to far far to your inner intestine</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
