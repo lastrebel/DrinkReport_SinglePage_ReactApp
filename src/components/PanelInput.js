9ed3b9ebf8ca6712185b9ee30dccc73c802e0d66
‘Brano <valasek.brano@gmail.com>
2021-07-10 15:30:46 +0200

    input creates array now, visible in console only

--- src/components/PanelInput.js
+++ src/components/PanelInput.js
@@ -1,12 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function PanelInput() {
+function PanelInput(props) {
+    const [input, setInput] = useState('    lola');
+    const handleChange = e => {
+        setInput(e.target.value)
+    }
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log('button clicked');
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 1000),
+            text: input
+        })
+
+        setInput('')
+    }
+
     return (
         <div class="panel3">
-            <form className="todo-form">
-                <input type="text" value="id" name="id" className='' /><input type="text" placeholder="Drink name"
-                    name="drink_name" className='' /><input type="text" placeholder="ml" name="value"
-                        className='' /><input type="text" value="now" name="now" className='' />
+            <form className="todo-form" onSubmit={handleSubmit}>
+                <input type="text" value="id" name="id" className='' />
+                <input type="text" placeholder="Drink name"
+                    name="drink_name" className='' value={input} onChange={handleChange} />
+                <input type="text" placeholder="ml" name="value"
+                    className='' />
+                <input type="text" value="now" name="now" className='' />
                 {/* <!--
                         <input type="text" placeholder="Drink name" value={input} name="text" className='todo-input edit'
                             onChange={handleChange} ref={inputRef} /> --> */}
--- src/components/Wrapper.js
+++ src/components/Wrapper.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PanelStatus from './PanelStatus';
 import PanelTable from './PanelTable';
 import PanelInput from './PanelInput';
 
 function Wrapper() {
+    const [records, setRecords] = useState([]);
 
+    const addRecord = record => {
+        if (!record.text || /^\s*$/.test(record.text)) {
+            return
+        }
+
+        const newRecords = [record, ...records]
+
+        setRecords(newRecords)
+        console.log(record, ...records)
+    }
     return (
         <div class="container">
             <h1>Drinking Report 2</h1>
             <PanelStatus />
             <PanelTable />
-            <PanelInput />
+            <PanelInput onSubmit={addRecord} />
         </div>
     )
 }
