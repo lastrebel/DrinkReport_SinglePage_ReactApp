f99ecb16d440569893907aad909d0fc073d979f4
‘Brano <valasek.brano@gmail.com>
2021-07-11 17:04:47 +0200

    fixed submitting all input fields

--- src/components/PanelInput.js
+++ src/components/PanelInput.js
@@ -1,31 +1,54 @@
 import React, { useState } from 'react'
 
 function PanelInput(props) {
-    const [input, setInput] = useState('    lola');
-    const handleChange = e => {
-        setInput(e.target.value)
+    var today = new Date();
+    //var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+    var time = today.getHours() + ":" + today.getMinutes();
+
+    const [state, setState] = useState({
+        item: '',
+        amount: '',
+        time: time,
+    });
+
+    function handleChange(e) {
+        // console.log(e.target.value); // works fine
+        // console.log(e.state);
+        const value = e.target.value;
+
+        setState({
+            ...state,
+            [e.target.name]: value
+        })
     }
+
     const handleSubmit = e => {
         e.preventDefault();
         console.log('button clicked');
 
         props.onSubmit({
             id: Math.floor(Math.random() * 1000),
-            text: input
+            item: state.item,
+            amount: state.amount,
+            time: state.time,
         })
 
-        setInput('')
+        setState({
+            item: '',
+            amount: '',
+            time: time,
+        })
     }
 
     return (
         <div class="panel3">
             <form className="todo-form" onSubmit={handleSubmit}>
-                <input type="text" value="id" name="id" className='' />
+                <input type="text" value="#2" name="id" className='' onChange={handleChange} />
                 <input type="text" placeholder="Drink name"
-                    name="drink_name" className='' value={input} onChange={handleChange} />
-                <input type="text" placeholder="ml" name="value"
-                    className='' />
-                <input type="text" value="now" name="now" className='' />
+                    name="item" className='' value={state.item} onChange={handleChange} />
+                <input type="text" placeholder="ml" name="amount"
+                    className='' value={state.amount} onChange={handleChange} />
+                <input type="text" name="time" className='' value={state.time} onChange={handleChange} />
                 {/* <!--
                         <input type="text" placeholder="Drink name" value={input} name="text" className='todo-input edit'
                             onChange={handleChange} ref={inputRef} /> --> */}
