94a313971da75cf0b192c9ee1f77a383f4c09e89
‘Brano <valasek.brano@gmail.com>
2021-08-20 21:02:52 +0200

    Part 2 - SUM has output in litres and shows under Current Status box

--- src/components/PanelStatus.js
+++ src/components/PanelStatus.js
@@ -1,16 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function PanelStatus({ dataFromParent }) {
     return (
         <div class="panel1">
             <div class="grow">
                 <div class="title">Status</div>
-                <div class="value">{dataFromParent}</div>
+                <div class="value">26% </div>
                 <div class="verbal">drink 8 more glasses</div>
             </div>
             <div class="">
                 <div class="title">Current Amount</div>
-                <div class="value">0.8 l gdsdfsdf sdfsdfs sdfsdv dgfsdf ddf</div>
+                <div class="value">{dataFromParent} l</div>
             </div>
             <div class="">
                 <div class="title">Expected Amount</div>
