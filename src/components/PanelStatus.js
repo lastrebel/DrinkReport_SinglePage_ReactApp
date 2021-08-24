bcfb25b17a5dcea5d8f19875b8bfd91a3c691a4d
‘Brano <valasek.brano@gmail.com>
2021-08-24 20:04:40 +0200

    PART 3 - status shows Status in percentage, errors like Infinity or NaN result into showwing message

--- src/components/PanelStatus.js
+++ src/components/PanelStatus.js
@@ -1,12 +1,36 @@
 import React, { useState } from 'react'
 
-function PanelStatus({ dataFromParent }) {
+function PanelStatus({ dataFromParent, handleChange }) {
+    //const [requested, setRequested] = useState();
+    const [requiredAmount, setRequiredAmount] = useState(0);
+    var status = Math.round((dataFromParent / requiredAmount) * 100)
+    var displayStatus
+    isNaN(status) || !Number.isFinite(status) ? displayStatus = 'Check Data Entries' : displayStatus = status + ' %'
+    function handleChange(e) {
+        e.preventDefault();
+        console.log('value changed ' + requiredAmount);
+        setRequiredAmount(e.target.value)
+        // getStatus()
+    }
+
+    // function getStatus() {
+    //     // const status = (dataFromParent / requiredAmount) * 100
+    //     return console.log(
+    //         '\nrequiredAmount type je ' + typeof (requiredAmount) +
+    //         '\nrequiredAmount je ' + requiredAmount +
+    //         '\ndataFromParent type je ' + typeof (dataFromParent) +
+    //         '\ndataFromParent je ' + dataFromParent +
+    //         '\nstatus type je ' + typeof (status) +
+    //         '\nstatus je ' + status)
+    // }
+
+
     return (
         <div class="panel1">
             <div class="grow">
                 <div class="title">Status</div>
-                <div class="value">26% </div>
-                <div class="verbal">drink 8 more glasses</div>
+                <div class="value">{displayStatus}</div>
+                {/* <div class="verbal">drink 8 more glasses</div> */}
             </div>
             <div class="">
                 <div class="title">Current Amount</div>
@@ -14,7 +38,7 @@ function PanelStatus({ dataFromParent }) {
             </div>
             <div class="">
                 <div class="title">Expected Amount</div>
-                <div class="value"><input value="2.5" placeholder="Enter value" /></div>
+                <div class="value"><input placeholder="Enter value" value={requiredAmount} onChange={handleChange} /></div>
             </div>
         </div>
     )
